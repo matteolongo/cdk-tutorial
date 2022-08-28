2f903d8e6575468178ebe04d2e631f2fdd1f67c0
Matteo Longo <matteo.longo@wallapop.com>
2022-08-29 01:09:16 +0200

    Test encryption db table

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -3,6 +3,22 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import {HitCounter} from '../lib/hitcounter';
 
+test('DynamoDB Table Created', () => {
+	const stack = new cdk.Stack();
+	// WHEN
+	new HitCounter(stack, 'MyTestConstruct', {
+		downstream: new lambda.Function(stack, 'TestFunction', {
+			runtime: lambda.Runtime.NODEJS_14_X,
+			handler: 'hello.handler',
+			code: lambda.Code.fromAsset('lambda')
+		})
+	});
+	// THEN
+
+	const template = Template.fromStack(stack);
+	template.resourceCountIs("AWS::DynamoDB::Table", 1);
+});
+
 test('Lambda Has Environment Variables', () => {
 	const stack = new cdk.Stack();
 	// WHEN
@@ -32,4 +48,23 @@ test('Lambda Has Environment Variables', () => {
 			},
 		}
 	);
+});
+
+test('DynamoDB Table Created With Encryption', () => {
+	const stack = new cdk.Stack();
+	// WHEN
+	new HitCounter(stack, 'MyTestConstruct', {
+		downstream: new lambda.Function(stack, 'TestFunction', {
+			runtime: lambda.Runtime.NODEJS_14_X,
+			handler: 'hello.handler',
+			code: lambda.Code.fromAsset('lambda')
+		})
+	});
+	// THEN
+	const template = Template.fromStack(stack);
+	template.hasResourceProperties('AWS::DynamoDB::Table', {
+		SSESpecification: {
+			SSEEnabled: true
+		}
+	});
 });
