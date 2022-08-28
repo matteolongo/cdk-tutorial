3d62ee5aaa04e0ce46185b859fb904b812482b74
Matteo Longo <matteo.longo@wallapop.com>
2022-08-28 14:46:18 +0200

    Add lambda

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,10 +1,16 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const hello = new lambda.Function(this, 'HelloHandler',{
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+    })
 
   }
 }
