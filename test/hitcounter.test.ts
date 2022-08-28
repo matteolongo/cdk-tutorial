8ae36eb9a0c6b686dbc4d537f52e91de07082cb4
Matteo Longo <matteo.longo@wallapop.com>
2022-08-29 01:04:45 +0200

    Test lambda env variables

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -1,9 +1,9 @@
-import {Template} from 'aws-cdk-lib/assertions';
+import {Capture, Template} from 'aws-cdk-lib/assertions';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import {HitCounter} from '../lib/hitcounter';
 
-test('DynamoDB Table Created', () => {
+test('Lambda Has Environment Variables', () => {
 	const stack = new cdk.Stack();
 	// WHEN
 	new HitCounter(stack, 'MyTestConstruct', {
@@ -14,7 +14,22 @@ test('DynamoDB Table Created', () => {
 		})
 	});
 	// THEN
-
 	const template = Template.fromStack(stack);
-	template.resourceCountIs("AWS::DynamoDB::Table", 1);
+	const envCapture = new Capture();
+	template.hasResourceProperties("AWS::Lambda::Function", {
+		Environment: envCapture,
+	});
+
+	expect(envCapture.asObject()).toEqual(
+		{
+			Variables: {
+				DOWNSTREAM_FUNCTION_NAME: {
+					Ref: "TestFunction22AD90FC",
+				},
+				HITS_TABLE_NAME: {
+					Ref: "MyTestConstructHits24A357F0",
+				},
+			},
+		}
+	);
 });
