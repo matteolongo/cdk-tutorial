778a7b4ab6bbd78af4ad0a27e3c48e4203c83079
Matteo Longo <matteo.longo@wallapop.com>
2022-08-28 15:24:35 +0200

    Create lambda

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -9,9 +9,22 @@ export interface HitCounterProps {
 export class HitCounter extends Construct {
 	constructor(scope: Construct, id: string, props: HitCounterProps) {
 		super(scope, id);
+
 		// create dynamodb table
 		const table = new dynamodb.Table(this, 'Hits', {
 			partitionKey: {name: 'path', type: dynamodb.AttributeType.STRING}
 		});
+
+		// create lambda
+		const hitLambda = new lambda.Function(this, 'HitCounterHandler', {
+			runtime: lambda.Runtime.NODEJS_14_X,
+			handler: 'hitcounter.handler',
+			code: lambda.Code.fromAsset('lambda'),
+			environment: {
+				DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+				HITS_TABLE_NAME: table.tableName
+			}
+
+		})
 	}
 }
